6f35712ea5d9ecdab7a11d2b2aac3af08f9218ca
kemely2018 <41025582+kemely2018@users.noreply.github.com>
2020-07-02 09:58:47 -0500

    Update trie5.cpp

--- Trie/trie5.cpp
+++ Trie/trie5.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#define MAX 100
 using namespace std;
 class TrieNode{
         private:
@@ -10,66 +12,82 @@ class TrieNode{
                     this->children[i] = NULL;
                 this->isLast = false;
             }
+            
         friend class Trie;
-    };
+};
 
 class Trie {
-private:
-    TrieNode* root;
-public:
-    /** Initialize your data structure here. */
+	public:
+		TrieNode* root;
+		Trie() {
+			root = new TrieNode();
+		}
+		~Trie(){
+			delete(root);
+		}
+		void insert(string word){
+			TrieNode* node = root;
+			int index;
+			for(int i = 0; i<word.length(); i++){
+				index = word[i]  - 'a';
+				if(node->children[index] == NULL){
+					node->children[index] = new TrieNode();
+				}
+				node = node->children[index];
+			}
+			node->isLast = true;	
+		}
+		
+		bool search(string word){
+			TrieNode* node = root;
+			int index;
+			for(int i = 0; i<word.length(); i++){
+				index = word[i]  - 'a';
+				node = node->children[index];
+				if(node == NULL){
+					return false;
+				}
+			}
+			return node->isLast;			
+		}
+		bool isLastNode(TrieNode* root){
+			return root->isLast!= false;
+		}
+		void display(TrieNode* root,char *str,int level){
+			
+			if (isLastNode(root)){
+				str[level] = '\0';
+				cout << str << endl; 
+			}
+			for (int i = 0; i <26; i++){
+				if (root->children[i]){
+					str[level] = i + 'a';
+					display(root->children[i],str, level + 1);
+				}
+			}
+		}
 
-    Trie() {
-        root = new TrieNode();
-    }
-    ~Trie(){
-        delete(root);
-    }
-    
-    /** Inserts a word into the trie. */
-    void insert(string word) {
-        TrieNode* node = root;
-        int index;
-        for(int i = 0; i<word.length(); i++){
-            index = word[i]  - 'a';
-            if(node->children[index] == NULL)
-                node->children[index] = new TrieNode();
-            node = node->children[index];
-        }
-        node->isLast = true;
-    }
-    
-    /** Returns if the word is in the trie. */
-    bool search(string word) {
-        TrieNode* node = root;
-        int index;
-        for(int i = 0; i<word.length(); i++){
-            index = word[i]  - 'a';
-            node = node->children[index];
-            if(node == NULL)
-                return false;
-        }
-        return node->isLast;
-    }
-    
-    /** Returns if there is any word in the trie that starts with the given prefix. */
-    bool startsWith(string prefix) {
-        TrieNode* node = root;
-        int index;
-        for(int i = 0; i<prefix.length(); i++){
-            index = prefix[i]  - 'a';
-            node = node->children[index];
-            if(node == NULL)
-                return false;
-        }    
-        return true;
-    }
 };
 
 
 int main(){
 	Trie* head = new Trie();
-	head->insert("hello");
-	cout << head->search("hello") << " ";
+    char *cad=NULL;
+    char *palabras =new char[MAX];
+    cad=new char[50];
+	ifstream f( "teste.txt" );
+	while( !f.eof() ) {
+		f.getline(cad,50);
+		string c(cad);
+		head->insert(c);
+    }
+    
+	f.close();
+	cout << head->search("abasedly") << " "<<endl;
+	int level=0;
+	//head->display(head->root,palabras,level);
+	
+	delete []cad;
+	delete []palabras;
 	return 0;
 }
