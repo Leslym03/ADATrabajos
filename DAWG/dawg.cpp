f8ce70cc38f90553b967a0222db547a248614c70
Leslym03 <42551036+Leslym03@users.noreply.github.com>
2020-07-21 21:39:19 -0500

    commit2

--- DAWG/dawg.cpp
+++ DAWG/dawg.cpp
@@ -16,9 +16,11 @@ private:
     bool fin;
     Estado *e_next;
     Transciones *t_inicio;
-
 public:
-
+    Estado(){
+        e_next=NULL;
+        t_inicio = NULL;
+    }
 }; 
 
 class Transciones{
@@ -41,27 +43,19 @@ private:
     vector<char> C;                //C es un alfabeto de cardenal finito
     //vector<Nodo> conjNodos;      
     //vector<Nodo> conNFinales;    //T es el subconjunto de estados terminales de Q
-    Estado *e_head;                //Q es un conjunto de estados    
-    Transciones *t_head;
-    
+    Estado *e_head;                //Q es un conjunto de estados      
 public: 
     DAWG(){
         e_head = NULL;
-        t_head = NULL;
     }
 
     ~DAWG(){
         Estado *aux1 = e_head;
         Estado *aux2;
-        Transciones *aux3 = t_head;
-        Transciones *aux4;
         while(aux1){
             aux2 = aux1->e_next;
             delete aux1;
             aux1 = aux2;
-            aux4 = aux3->t_next;
-            delete aux3;
-            aux3 = aux4;
         }
     }
 
@@ -122,49 +116,44 @@ public:
     //F es una función de Q x C en Q que define las transiciones (arcos) del autómata
     void insertar_trans(int actual, int next, char aris){
         Transciones *trans = new Transciones;
-        trans->e_actual = buscar_est(actual);
+        Estado *estado_trans = buscar_est(actual);
+        trans->e_actual = estado_trans;
         trans->e_next = buscar_est(next);
         trans->letra = aris;
 
         Transciones *aux1;
         Transciones *aux2;
-        aux1 = t_head;
+        aux1 = estado_trans->t_inicio;
         while(aux1){
             aux2 = aux1;
             aux1 = aux1->t_next;
         }
-        if(t_head == aux1)
-            t_head = trans;
+        if(estado_trans->t_inicio == aux1)
+            estado_trans->t_inicio = trans;
         else
             aux2->t_next = trans;
         trans->t_next = aux1;
     }
 
     void mostrar_trans(){
-        Transciones *aux1=t_head;
-        while(aux1){
-            cout << aux1->e_actual->num << " " << aux1->letra << " " << aux1->e_next->num << endl;
-            aux1=aux1->t_next;
-        }
-        cout<<endl;
+        
     }
 
     void altura(){
-
+        
     }
-
 }; 
   
 
 int main(){
     DAWG a;
-    /**
+
     vector<char> alf;
     alf.push_back('a');
     alf.push_back('b');
     alf.push_back('c');
     a.alfabetoC(alf);
-    **/
+
     a.insertar_est(1);
     a.insertar_est(2);
     a.insertar_est(3);
@@ -174,22 +163,19 @@ int main(){
     a.insertar_est(7);
     a.insertar_est(8);
     a.mostrar_est();
-
-    
+    /**
     a.insertar_trans(1,2,'a');
     a.insertar_trans(1,3,'b');
     a.insertar_trans(1,4,'c');
-
     a.insertar_trans(2,5,'a');
     a.insertar_trans(2,6,'b');
     a.insertar_trans(3,5,'a');
     a.insertar_trans(3,6,'b');
     a.insertar_trans(4,7,'a');
     a.insertar_trans(4,8,'c');
-
     a.insertar_trans(5,8,'a');
     a.insertar_trans(6,8,'b');
-    
-    a.mostrar_trans();
+    **/
+    //a.mostrar_trans();
     return 0;
 }
